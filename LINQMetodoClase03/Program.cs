34209ce42458f15fa9f2c53d04539dfffce14921
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-30 09:22:12 -0500

    Query Linq desde metodos

---
+++ LINQMetodoClase03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQMetodoClase03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Obtener resultados de query desde metodos
+
+            // Invocamos el metodo
+            IEnumerable<int> resultados = CClaseExplicita.ObtenerNumerosPares();
+
+            // Mostramos los resultados 
+            foreach (int par in resultados)
+                Console.WriteLine(par);
+
+            Console.WriteLine("- - - - - -");
+
+            IEnumerable<string> postres = CClaseExplicita.ObtenerPostres();
+
+            // Mostramos los resultados
+            foreach(string postre in postres)
+                Console.WriteLine(postre);
+
+            Console.WriteLine("- - - - - -");
+
+            // Ejemplo de resultado de query inmediato
+            int[] impares = CClaseExplicita.ObtenerNumerosImpares();
+
+            // Mostramos los resultados
+            foreach(int impar in impares)
+                Console.WriteLine(impar);
+        }
+    }
+}
